d17e853b28752e46c390af7f6c7b704b711cccd0
hirotosakurai <k021g1130@g.neec.ac.jp>
2022-11-11 09:34:44 +0900

    02_03_授業内容

--- Object3d.h
+++ Object3d.h
@@ -42,8 +42,8 @@ private: // 定数
 	static const float radius;				// 底面の半径
 	static const float prizmHeight;			// 柱の高さ
 	static const int planeCount = division * 2 + division * 2;		// 面の数
-	static const int vertexCount = 4;		// 頂点数
-	static const int indexCount = 3 * 2; //インディックス数
+	static const int vertexCount = 1;		// 頂点数
+	//static const int indexCount = 3 * 2; //インディックス数
 
 public: // 静的メンバ関数
 	/// <summary>
@@ -118,8 +118,8 @@ private: // 静的メンバ変数
 	static ComPtr<ID3D12DescriptorHeap> descHeap;
 	// 頂点バッファ
 	static ComPtr<ID3D12Resource> vertBuff;
-	// インデックスバッファ
-	static ComPtr<ID3D12Resource> indexBuff;
+	//// インデックスバッファ
+	//static ComPtr<ID3D12Resource> indexBuff;
 	// テクスチャバッファ
 	static ComPtr<ID3D12Resource> texbuff;
 	// シェーダリソースビューのハンドル(CPU)
@@ -138,12 +138,12 @@ private: // 静的メンバ変数
 	static XMFLOAT3 up;
 	// 頂点バッファビュー
 	static D3D12_VERTEX_BUFFER_VIEW vbView;
-	// インデックスバッファビュー
-	static D3D12_INDEX_BUFFER_VIEW ibView;
+	//// インデックスバッファビュー
+	//static D3D12_INDEX_BUFFER_VIEW ibView;
 	// 頂点データ配列
 	static VertexPosNormalUv vertices[vertexCount];
-	// 頂点インデックス配列
-	static unsigned short indices[indexCount];
+	//// 頂点インデックス配列
+	//static unsigned short indices[indexCount];
 	//ビルボート行列
 	static XMMATRIX matBillboard;
 	//Y軸回りののビルボード
--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,33 +1,25 @@
 #include "BasicShaderHeader.hlsli"
 
-//三角形の入力から、線分を3つ出力するサンプル
-[maxvertexcount(6)]
+//点の入力から、三角形を出力
+[maxvertexcount(3)]
 void main(
-	triangle VSOutput input[3] : SV_POSITION,
+	point VSOutput input[1] : SV_POSITION,
 	//線分ストリーム
 	inout TriangleStream< GSOutput > output
 )
 {
-	//1つ目の三角形
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput element; //出力用頂点データ
-		element.svpos = input[i].svpos; //頂点座標をコピー
-		element.normal = input[i].normal; //法線をコピー
-		element.uv = input[i].uv; //UVをコピー
-		output.Append(element);
-	}
-	//現在のストリップを終了
-	output.RestartStrip();
-	//2つ目の三角形
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput element;
-		//X方向に20ずらす
-		element.svpos = input[i].svpos + float4(20.0f, 0.0f, 0.0f,0.0f); //頂点座標をコピー
-		element.normal = input[i].normal; //法線をコピー
-		//UVを5倍に
-		element.uv = input[i].uv * 5.0f; //UVをコピー
-		output.Append(element);
-	}
+	GSOutput element; //出力用頂点データ
+	//共通
+	element.normal = input[0].normal;
+	element.uv = input[0].uv;
+	//1点目
+	element.svpos = input[0].svpos;
+	output.Append(element);
+	//2点目
+	element.svpos = input[0].svpos + float4(10.0f, 10.0f, 0, 0);
+	output.Append(element);
+	//3点目
+	element.svpos = input[0].svpos + float4(10.0f, 0, 0, 0);
+	output.Append(element);
 }
+	
